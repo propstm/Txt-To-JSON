e5f483d2998d3b45c1e0273c48dd56f063267538
propstm <propstm@gmail.com>
2023-08-08 10:14:15 -0400

    Updated script to convert JSON to Arb file

--- app.js
+++ app.js
@@ -39,16 +39,12 @@ const checkForTrailingComma = (input) => {
 
 const writeOutput = () => {
   console.log('+++ writing lines start');
-  var lineWriter = fs.createWriteStream(`output/homeskoolin-${epNumber}-transcript.json`, {
+  var lineWriter = fs.createWriteStream(`output/intl_en.arb`, { // not always English
     flags: 'a' // 'a' means appending (old data will be preserved)
   })
-  lineWriter.write('{');
-  lineWriter.write('"Episode Number":"'+ epNumber + '",');
-  lineWriter.write('"Episode Title":'+ epTitle + ',');
-  lineWriter.write('"URL":"'+ epUrl + '",');
-  lineWriter.write('"Content":['); 
+  lineWriter.write('{\n');
+  lineWriter.write('"@@locale": "en",') // this line changes based on the language translated
   lineWriter.write(innerContent);
-  lineWriter.write(']'); 
   lineWriter.write('}');
   lineWriter.end();
   console.log('++++ writing lines end');
@@ -56,29 +52,31 @@ const writeOutput = () => {
 
 const readLinesForContent = () =>{
   var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(`input/${epNumber}.txt`)
+    input: require('fs').createReadStream(`input/${epNumber}`)
   });
   let currentLine = 1;
   
   console.log('starting line reading');
   lineReader.on('line', function (line) {
-  
-    let formedLine = '"'+line+'"';
-  
-    if(currentLine % 2 == 1){
-      formedLine ='"Timestamp":'+formedLine+',';
-      //console.log('{\n' + formedLine);
-      innerContent = innerContent + '{\n' + formedLine;
-  
+    //Need to modify data in json k/v pairs to be Arb file compliant 
+    /*
+      - Split k/v pairs
+      - Update key to be camelCased and remove period
+    */
+
+    if(line.includes('"')){
+      var kvSplit = line.split(':');
+      var oldKey = kvSplit[0];
+      var newKey = oldKey.split('.').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join('');
+      var newLine = newKey+':'+kvSplit[1];
+      console.log(newLine);  
+
+      //Output those lines
+      let formedLine = newLine;
+      innerContent = innerContent + formedLine+ '\n';
+      currentLine++;
     }
-  
-    if(currentLine % 2 == 0){
-      formedLine ='"Text":'+formedLine;
-      //console.log(formedLine + '},\n');
-      innerContent = innerContent + formedLine+ '\n},\n';
-    }
-  
-    currentLine++;
+
   
   }).on('close', () => {
 
